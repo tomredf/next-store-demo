66f7eed9c9e2d46b6a3ad08d52aeae19dc2b003b
Dermot Fitzpatrick <39203833+tomredf@users.noreply.github.com>
2020-12-26 15:31:56 -0800

    ...

--- pages/search.tsx
+++ pages/search.tsx
@@ -99,6 +99,7 @@ export default function Search({
                   {activeCategory?.name
                     ? `Category: ${activeCategory?.name}`
                     : 'All Categories'}
+                    
                   <svg
                     className="-mr-1 ml-2 h-5 w-5"
                     xmlns="http://www.w3.org/2000/svg"
